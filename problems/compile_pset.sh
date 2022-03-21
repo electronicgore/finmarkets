27da3bd3bdb09946a411d4e022b76963e1c2074e
e s <egor@starkov.email>
2022-03-21 13:15:13 +0100

    fix compilation script because solutions are defined differently in these psets

--- problems/compile_pset.sh
+++ problems/compile_pset.sh
@@ -15,6 +15,7 @@ OUT=${2:-$1}
 cp $1.tex $1.tex.bak
 
 sed -i 's/%\\solutionstrue/\\solutionstrue/' $1.tex
+sed -i 's/%, answers/, answers/' $1.tex
 pdflatex -synctex=1 -interaction=nonstopmode $1.tex
 pdflatex -synctex=1 -interaction=nonstopmode $1.tex
 #pdfjam $1.pdf --nup '1x2' --frame 'true' --outfile ${OUT}_handout.pdf
@@ -23,12 +24,14 @@ echo 'Solutions compiled.'
 
 
 sed -i 's/\\solutionstrue/%\\solutionstrue/' $1.tex
+sed -i 's/, answers/%, answers/' $1.tex
 pdflatex -synctex=1 -interaction=nonstopmode $1.tex
 pdflatex -synctex=1 -interaction=nonstopmode $1.tex
 cp $1.pdf ${OUT}_p.pdf
 echo 'Pset compiled.'
 
 sed -i 's/%\\solutionstrue/\\solutionstrue/' $1.tex
+sed -i 's/%, answers/, answers/' $1.tex
 
 echo 'All done.'
 
